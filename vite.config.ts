101f96e6f0f5661a8cb78344c2d047b0665b6e57
Andrew floatrx <ifloatrx@gmail.com>
2024-05-29 21:30:29 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,24 +5,24 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      manifest: {
-        start_url: '.',
-        display: 'standalone',
-        theme_color: '#06040C',
-        background_color: '#06040C',
-        icons: [
-          {src: 'favicon.svg', type: 'image/svg+xml'},
-          {src:'favicon.png', sizes: '32x32', type: 'image/png'},
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,svg,mp3}'],
-        globIgnores: ['**/*.map'],
-        skipWaiting: true,
-        clientsClaim: true,
-        cleanupOutdatedCaches: true,
-      },
-    }),
+    // VitePWA({
+    //   manifest: {
+    //     start_url: '.',
+    //     display: 'standalone',
+    //     theme_color: '#06040C',
+    //     background_color: '#06040C',
+    //     icons: [
+    //       {src: 'favicon.svg', type: 'image/svg+xml'},
+    //       {src:'favicon.png', sizes: '32x32', type: 'image/png'},
+    //     ]
+    //   },
+    //   workbox: {
+    //     globPatterns: ['**/*.{js,css,html,svg,mp3}'],
+    //     globIgnores: ['**/*.map'],
+    //     skipWaiting: true,
+    //     clientsClaim: true,
+    //     cleanupOutdatedCaches: true,
+    //   },
+    // }),
   ],
 })
